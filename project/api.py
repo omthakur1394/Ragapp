c28afa6771c1cecfa4071282996360f92df9381e
omthakur1394 <omthakur1394@gmail.com>
2026-01-19 13:09:12 +0530

    remove the end point

--- project/api.py
+++ project/api.py
@@ -43,6 +43,3 @@ def chat(req: ChatRequest):
     chat_history.append(AIMessage(content=response))
 
     return {"answer": response}
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
